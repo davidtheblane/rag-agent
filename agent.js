2c758b0be63003801fe4ef4b8a756b61e07178f8
Davi <davidtheblane@gmail.com>
2025-06-09 14:47:39 -0300

    AI Agent Rest API

--- agent.js
+++ agent.js
@@ -1,45 +1,29 @@
-// import { ChatAnthropic } from "@langchain/anthropic";
-import { createReactAgent } from "@langchain/langgraph/prebuilt";
+import { createReactAgent, } from "@langchain/langgraph/prebuilt";
+import { MemorySaver } from "@langchain/langgraph";
 import { ChatOllama } from "@langchain/ollama";
-// from langchain_community.chat_models import ChatOllama
+import { tool } from "@langchain/core/tools"
 import { z } from 'zod'
 
-import { tool } from "@langchain/core/tools"
 
 const weatherTool = tool(async ({ query }) => {
     console.log('query', query)
-    return 'The weather is sunny'
+    return 'The weather in Tokyo is sunny';
 }, {
     name: 'weather',
     description: 'Get the weather in a given location.',
     schema: z.object({
-        query: z.string().describe('The location to get the weather for.')
+        query: z.string().describe('The query to use in search')
     })
 })
 
-// import { tool } from "@langchain/core/tools";
-// import { z } from "zod";
-
-// Define the model
-const chat = new ChatOllama({
+const llm = new ChatOllama({
     model: "llama3.2:1b",
 });
 
-// const chat = new ChatOllama({ model: "llama3.2:1b" });
+const checkpointSaver = new MemorySaver();
 
-// const agent = createReactAgent({
-//     model: model,
-//     tools: []
-// })
-
-const result = (await chat.invoke([
-    [
-        "system",
-        "Voce é um engenheiro de dados especialista em desenvolvimento, IA e Devops",
-    ],
-    ["human", "O que é aTOTVS?"],
-])).content;
-
-result;
-
-console.log(result);
+export const agent = createReactAgent({
+    llm,
+    tools: [weatherTool],
+    checkpointSaver,
+})
